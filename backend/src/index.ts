1553fb0220a93baf14335f34403f73873b39b37f
Lorenzo Dall'Ara <lorenzo.dallara2@studio.unibo.it>
2025-05-10 18:00:07 +0200

    Implemented inventory and available contracts api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,8 +2,8 @@ import Express from "express";
 import BodyParser from "body-parser";
 import dotenv from "dotenv";
 import { drizzle } from "drizzle-orm/mysql2"
-import { eq, desc } from "drizzle-orm";
-import { upgradeTypes, login, player, friendship } from "./db/schema";
+import { eq, desc, notInArray, inArray } from "drizzle-orm";
+import { upgradeTypes, login, player, friendship, playerBullet, playerMedikit, playerWeapon, playerUpgrade, employedMercenary, assignedMercenary, activeContract, contract } from "./db/schema";
 import bcrypt from "bcrypt" // bcrypt for hashing passwords
 import { v7 as uuidv7 } from 'uuid'; // uuidv7 for generating unique tokens
 
@@ -14,37 +14,53 @@ const db = drizzle(process.env.DATABASE_URL!)
 const SALT_ROUNDS = 12
 
 app.post("/auth/tokenLogin", (req, res) => {
-    let { idPlayer, token } = req.body
-    if (!idPlayer || !token) {
+    let token = req.body
+    if (!token) {
         res.status(400).send()
     }
     // Check if user exists in the database
-    db.select()
-        .from(login)
-        .where(eq(login.idPlayer, idPlayer))
-        .limit(1)
-        .execute()
-        .then((user) => {
-            if (user.length > 0) {
-                // Check if the token is valid
-                if (user[0].authToken !== token) {
-                    res.status(401).send()
-                } else {
+    getPlayerByToken(token).then((tokenUser) => {
+        if (tokenUser) {
+            db.select()
+            .from(login)
+            .where(eq(login.idPlayer, tokenUser.idPlayer))
+            .limit(1)
+            .execute()
+            .then((user) => {
+                if (user.length > 0) {
                     const newToken = uuidv7()
                     // Save the new token in the database
                     saveToken(user[0].email, newToken)
                     // Send the new token back to the user
-                    res.status(200).json({ idPlayer: user[0].idPlayer, authToken: newToken })
+                    res.status(200).json({ authToken: newToken })
+                } else {
+                    res.status(400).send()
                 }
+            })
+            .catch((error) => {
+                console.error(error)
+                res.status(500).send()
+            })
+        } else {
+            res.status(400).send()
+        }
+    })
+})
+
+function getPlayerByToken(token: string) {
+    return db.select()
+        .from(login)
+        .where(eq(login.authToken, token))
+        .limit(1)
+        .execute()
+        .then((user) => {
+            if (user.length > 0) {
+                return user[0]
             } else {
-                res.status(400).send()
+                return null
             }
         })
-        .catch((error) => {
-            console.error(error)
-            res.status(500).send()
-        })
-})
+}
 
 app.post("/auth/login", (req, res) => {
     console.log("RECEIVED LOGIN REQUEST")
@@ -146,28 +162,34 @@ async function insertUser(email: string, password: string, username: string) {
     return { inserted: result, id: ids[0].id, token: token }
 }
 
-app.get("bounty/friends/:idPlayer/:token", (req, res) => {
+app.get("bounty/friends/", (req, res) => {
     // Get the friends of the player from the database
-    const idPlayer =  Number(req.params.idPlayer)
-    const token = req.params.token as string
-    if (!idPlayer || !token) {
+    const token = req.body.authToken
+    if (!token) {
         res.status(400).send()
     } else {
-        verifyWithToken(idPlayer, token).then((verified) => {
+        verifyToken(token).then((verified) => {
             if (verified) {
-                db.select()
-                    .from(friendship)
-                    .where(eq(friendship.idPlayerFrom, idPlayer))
-                    .fullJoin(player, eq(friendship.idPlayerFriend, player.id))
-                    .orderBy(desc(player.bounty))
-                    .execute()
-                    .then((friends) => {
-                        res.status(200).json(friends)
-                    })
-                    .catch((error) => {
-                        console.error(error)
-                        res.status(500).send()
-                    })
+                getPlayerByToken(token).then((tokenUser) => {
+                    if(tokenUser) {
+                        // Get the friends of the player from the database
+                        db.select()
+                        .from(friendship)
+                        .where(eq(friendship.idPlayerFrom, tokenUser.idPlayer))
+                        .fullJoin(player, eq(friendship.idPlayerFriend, player.id))
+                        .orderBy(desc(player.bounty))
+                        .execute()
+                        .then((friends) => {
+                            res.status(200).json(friends)
+                        })
+                        .catch((error) => {
+                            console.error(error)
+                            res.status(500).send()
+                        })
+                    } else {
+                        res.status(400).send()
+                    }
+                })
             } else {
                 res.status(401).send()
             }
@@ -175,20 +197,14 @@ app.get("bounty/friends/:idPlayer/:token", (req, res) => {
     }
 })
 
-function verifyWithToken(idPlayer: number, token: string) {
-    // Check if the user exists in the database
+function verifyToken(token: string) {
     return db.select()
         .from(login)
-        .where(eq(login.idPlayer, idPlayer))
+        .where(eq(login.authToken, token))
         .limit(1)
         .execute()
         .then((user) => {
-            if (user.length > 0) {
-                // Check if the token is valid
-                return user[0].authToken === token
-            } else {
-                return false
-            }
+            return user.length > 0
         })
         .catch((error) => {
             console.error(error)
@@ -213,8 +229,149 @@ app.get("/bounty/leaderboard", (req, res) => {
 
 app.get("/inventory/", (req, res) => {
     // Get the inventory of the player from the database")
-    const idPlayer = Number(req.query.idPlayer)
-
+    const token = req.body.authToken
+    if (!token) {
+        res.status(400).send()
+    }
+    verifyToken(token).then((verified) => {
+        if (verified) {
+            getPlayerByToken(token).then((tokenUser) => {
+                if(tokenUser) {
+                    const bullets = getPlayerBullets(tokenUser.idPlayer)
+                    const medikits = getPlayerMedikits(tokenUser.idPlayer)
+                    const weapons = getPlayerWeapons(tokenUser.idPlayer)
+                    const upgrades = getPlayerUpgrades(tokenUser.idPlayer)
+                    Promise.all([bullets, medikits, weapons, upgrades]).then((results) => {
+                        res.status(200).json({
+                            bullets: results[0],
+                            medikits: results[1],
+                            weapons: results[2],
+                            upgrades: results[3]
+                        })
+                    }).catch((error) => {
+                        console.error(error)
+                        res.status(500).send()
+                    }).catch((error) => {
+                        console.error(error)
+                        res.status(500).send()
+                    })
+                } else {
+                    res.status(400).send()
+                }
+            })
+        } else {
+            res.status(401).send()
+        }
+    })
 })
+
+app.get("/contracts/available", (req, res) => {
+    // Get the available contracts from the database
+    const token = req.body.authToken
+    if (!token) {
+        res.status(400).send()
+    }
+    verifyToken(token).then((verified) => {
+        if (verified) {
+            getPlayerByToken(token).then((tokenUser) => {
+                if(tokenUser) {
+                    const available = getActiveContracts(tokenUser.idPlayer).then((activeContracts) => {
+                        const query = db.select().from(contract)
+                        if(activeContracts) {
+                            return query
+                                .where(notInArray(contract.id, activeContracts.map((c) => c.id)))
+                                .execute()
+                        } else {
+                            return query.execute()
+                        }
+                    })
+                    available.then((a) => res.status(200).json(a)
+                )
+                } else {
+                    res.status(400).send()
+                }
+            })
+        } else {
+            res.status(401).send()
+        }
+    })
+})
+
+function getEmployedMercenaries(idPlayer: number) {
+    return db.select()
+        .from(employedMercenary)
+        .where(eq(employedMercenary.idPlayer, idPlayer))
+        .execute()
+        .then((mercenaries) => {
+            if (mercenaries.length > 0) {
+                return mercenaries
+            } else {
+                return null
+            }
+        })
+}
+
+function getAssignedMercenaries(idPlayer: number) {
+    return getEmployedMercenaries(idPlayer).then((mercenaries) => {
+        if (mercenaries) {
+            return db.select()
+                .from(assignedMercenary)
+                .where(inArray(assignedMercenary.idEmployedMercenary, mercenaries.map((m) => m.idMercenary)))
+                .execute()
+                .then((assigned) => {
+                    return assigned
+                })
+        } else {
+            return null
+        }
+    })
+}
+
+function getActiveContracts(idPlayer:number) {
+    return getAssignedMercenaries(idPlayer).then((mercenaries) => {
+        if(mercenaries) {
+            const contracts = mercenaries.map((mercenary) => {
+                return db.select()
+                .from(activeContract)
+                .where(eq(activeContract.id, mercenary.idActiveContract))
+                .execute()
+                .then((assigned) => {
+                    return assigned
+                })
+            })
+            return Promise.all(contracts).then((active) => active.flat())
+        } else {
+            return null
+        }
+    })
+}
+
+function getPlayerBullets(idPlayer: number) {
+    return db.select()
+        .from(playerBullet)
+        .where(eq(playerBullet.idPlayer, idPlayer))
+        .execute()
+}
+
+function getPlayerMedikits(idPlayer: number) {
+    return db.select()
+        .from(playerMedikit)
+        .where(eq(playerMedikit.idPlayer, idPlayer))
+        .execute()
+}
+
+function getPlayerWeapons(idPlayer: number) {
+    return db.select()
+        .from(playerWeapon)
+        .where(eq(playerWeapon.idPlayer, idPlayer))
+        .execute()
+}
+
+function getPlayerUpgrades(idPlayer: number) {
+    return db.select()
+        .from(playerUpgrade)
+        .where(eq(playerUpgrade.idPlayer, idPlayer))
+        .execute()
+}
 
 app.listen(process.env.PORT)
