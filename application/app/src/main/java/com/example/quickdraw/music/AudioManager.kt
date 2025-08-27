57231f276f07a31beb044f38ee72be4c66da35c3
Giacomo Boschi <giacomo.boschi7@studio.unibo.it>
2025-08-27 15:00:01 +0200

    New audios for settings

--- application/app/src/main/java/com/example/quickdraw/music/AudioManager.kt
+++ application/app/src/main/java/com/example/quickdraw/music/AudioManager.kt
@@ -10,8 +10,24 @@ import com.example.quickdraw.R
 import com.example.quickdraw.TAG
 
 //https://pixabay.com/music/traditibgmonal-country-cowboy-western-background-247644/
+//https://pixabay.com/music/rock-western-rock-252363/
 //https://pixabay.com/sound-effects/gunshot-372470/
+//https://pixabay.com/music/modern-country-western-165285/
+enum class SONGS{
+    BACKGROUND_1,
+    BACKGROUND_2,
+    BACKGROUND_3,
+    LOBBY_THEME
+}
+
 object AudioManager: DefaultLifecycleObserver {
+
+    private var mappedSongs = mapOf<Int,Int>(
+        Pair(SONGS.BACKGROUND_1.ordinal,R.raw.background_theme),
+        Pair(SONGS.BACKGROUND_2.ordinal,R.raw.background_theme2),
+        Pair(SONGS.BACKGROUND_3.ordinal,R.raw.background_theme3),
+    )
+
     private var bgm: MediaPlayer? = null
     private var sfx: MediaPlayer? = null
 
@@ -26,6 +42,17 @@ object AudioManager: DefaultLifecycleObserver {
            sfx = MediaPlayer.create(context, R.raw.gunshot)
            sfx!!.isLooping = false
         }
+    }
+
+    fun changeBgmTheme(context: Context, choice:Int){
+        if (mappedSongs.containsKey(choice) && mappedSongs[choice]!=null){
+            bgm = MediaPlayer.create(context,mappedSongs[choice]!!)
+            bgm!!.isLooping = true
+        }
+    }
+
+    fun changeBgmTheme(context: Context, choice:SONGS){
+        changeBgmTheme(context,choice.ordinal)
     }
 
     fun startSFX(){
