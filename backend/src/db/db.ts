9f62a405352d81a40c5a320eebc1d2ac101ae8b5
MatteoGodzilla <catena.matteo90@gmail.com>
2025-09-01 10:30:34 +0200

    removed old backend

--- backend/src/db/db.ts
+++
@@ -1,8 +0,0 @@
-import dotenv from "dotenv";
-import { drizzle } from "drizzle-orm/mysql2"
-
-dotenv.config()
-console.log(process.env.DATABASE_URL);
-const db = drizzle(process.env.DATABASE_URL!)
-
-export default db;
